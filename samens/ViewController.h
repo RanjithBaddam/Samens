ae12f511938a86eb3218fbffbd0d69f2ffaaa456
All time Support <alltimesupport@Alls-MacBook-Pro.local>
2017-05-30 14:55:22 +0530

    Initial Commit

---
+++ samens/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  samens
+//
+//  Created by All time Support on 30/05/17.
+//  Copyright © 2017 All time Support. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ samens/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  samens
+//
+//  Created by All time Support on 30/05/17.
+//  Copyright © 2017 All time Support. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
